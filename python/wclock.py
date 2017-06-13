31c3a7332105541843828110eb36cad7cf6d2caf
xdaks <xdaks@seznam.cz>
2017-06-13 20:01:16 +0200

    source code reworked

--- python/wclock.py
+++ python/wclock.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+#
 #  This file is part of Word Clock
 #
 #            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 #                    Version 2, December 2004
 #
-# Copyright (C) 2016 xDaks <http://xdaks.deviantart.com/>
+# Copyright (C) 2017 xDaks <http://xdaks.deviantart.com/>
 #
 # Everyone is permitted to copy and distribute verbatim or modified
 # copies of this license document, and changing it is allowed as long
@@ -21,12 +22,11 @@
 import sys
 import datetime
 
-def getFace():
 
+def getFace():
     # -------------------------------------------------------------------- #
     # vars
     # -------------------------------------------------------------------- #
-
     class APP:
         face = '\n'.join([
             "[I T ' S] X [A B O U T] E",
@@ -48,6 +48,22 @@ def getFace():
     HOUR = t.hour % 12 or 12
     MINUTE = t.minute
 
+    CNames = ['I T \' S', 'A B O U T', 'A', 'Q U A R T E R', 'T W E N T Y', 'F I V E', 'F I V E', 'T E N',
+              'N I N E', 'O N E', 'S I X', 'T H R E E', 'F O U R', '*F I V E*',
+              'T W O', 'E I G H T', 'E L E V E N', 'S E V E N', 'T W E L V E',
+              'T E N', 'O C L O C K', 'P A S T', 'H A L F', 'T O']
+
+    CPre = [[-1, -1], [-1, -1]]
+
+    CTimes = ([[15, 20], [15, 20], [20, 30], [25, 30, False], [5, 10], [10, 15],
+               [9], [1], [6], [3], [4], [5], [2], [8], [11], [7], [12], [10]]
+           if MINUTE < 35 else
+              [[45, 50], [45, 50], [35, 45], [35, 40, False], [55, 61], [50, 55],
+               [8], [12], [5], [2], [3], [4], [1], [7], [10], [6], [11], [9]])
+
+    CPost = [[0, 5], [5, 35], [30, 35], [35, 61]]
+
+    cTimeData = CPre + CTimes + CPost
 
     if (__name__ == '__main__'):
         if len(sys.argv) > 1 and sys.argv[1] == '-t':
@@ -72,7 +88,6 @@ def getFace():
     # -------------------------------------------------------------------- #
     # functions
     # -------------------------------------------------------------------- #
-
     def fmtFace(match, val, repl):
         src = '[{0}]'.format(val)
         if match:
@@ -80,11 +95,11 @@ def getFace():
         elif repl:
             APP.face = APP.face.replace(src, fmt_off.format(val), 1)
 
-    def minFMT(nMinFrom, nMinTo, val, repl = True):
+    def minFMT(val, nMinFrom, nMinTo, repl = True):
         match = (nMinFrom <= MINUTE < nMinTo) or (nMinFrom == -1)
         fmtFace(match, val, repl)
 
-    def hourFMT(nHour, val, repl = True):
+    def hourFMT(val, nHour, repl = True):
         match = (nHour == HOUR) or (nHour == 0)
         fmtFace(match, val, repl)
 
@@ -94,54 +109,13 @@ def getFace():
     # -------------------------------------------------------------------- #
     # actions
     # -------------------------------------------------------------------- #
+    for i, val in enumerate(cTimeData):
+        isHour = len(val) == 1
+        txt = CNames[i]
+        hourFMT(txt, *val) if isHour else minFMT(txt, *val)
 
-    def buidFace():
-        minFMT(-1, -1, "I T ' S")
-        minFMT(-1, -1, 'A B O U T')
+    fiveFMT()
 
-        if MINUTE < 35:
-            [minFMT(*mi) for mi in [
-                (15, 20, 'A'),
-                (15, 20, 'Q U A R T E R'),
-                (20, 30, 'T W E N T Y'),
-                (25, 30, 'F I V E', False),
-                (5, 10, 'F I V E'),
-                (10, 15, 'T E N')
-            ]]
-
-            [hourFMT(*hour) for hour in [
-                (9, 'N I N E'),    (1, 'O N E'),         (6, 'S I X'),
-                (3, 'T H R E E'),  (4, 'F O U R'),       (5, '*F I V E*'),
-                (2, 'T W O'),      (8, 'E I G H T'),     (11, 'E L E V E N'),
-                (7, 'S E V E N'),  (12, 'T W E L V E'),  (10, 'T E N')
-            ]]
-        else:
-            [minFMT(*mi) for mi in [
-                (45, 50, 'A'),
-                (45, 50, 'Q U A R T E R'),
-                (35, 45, 'T W E N T Y'),
-                (35, 40, 'F I V E', False),
-                (55, 61, 'F I V E'),
-                (50, 55, 'T E N')
-            ]]
-
-            [hourFMT(*hour) for hour in [
-                (8, 'N I N E'),    (12, 'O N E'),        (5, 'S I X'),
-                (2, 'T H R E E'),  (3, 'F O U R'),       (4, '*F I V E*'),
-                (1, 'T W O'),      (7, 'E I G H T'),     (10, 'E L E V E N'),
-                (6, 'S E V E N'),  (11, 'T W E L V E'),  (9, 'T E N')
-            ]]
-
-        [minFMT(*mi) for mi in [
-            (0, 5, 'O C L O C K'),
-            (5, 35, 'P A S T'),
-            (30, 35, 'H A L F'),
-            (35, 61, 'T O')
-        ]]
-
-        fiveFMT()
-
-    buidFace()
     return APP.face
 
 def Face():

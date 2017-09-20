a564ae5e3fcca67457b89f8e7c9e511ca3b8201f
xdaks <xdaks@seznam.cz>
2017-09-20 20:07:24 +0200

    code refactoring

--- html/wclock.es6.js
+++ html/wclock.es6.js
@@ -16,7 +16,7 @@ TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
 ----------------------------------------------------------------------------- */
 
-let timeVal = '';
+var timeVal = '';
 
 function getFace() {
 
@@ -30,65 +30,11 @@ function getFace() {
 
     const MINUTE = t.getMinutes();
 
-    let face = [
-            "(I T ' S) X (A B O U T) E",
-            "(A) C (Q U A R T E R) D C",
-            "(T W E N T Y) (F I V E) X",
-            "(H A L F) B (T E N) F (T O)",
-            "(P A S T) E R U (N I N E)",
-            "(O N E) (S I X) (T H R E E)",
-            "(F O U R) (F I V E) (T W O)",
-            "(E I G H T) (E L E V E N)",
-            "(S E V E N) (T W E L V E)",
-            "(T E N) S E (O C L O C K)"
-    ];
-
-    const cTimes = MINUTE < 35 ? [
-        [[-1, -1], [-1, -1]],
-        [[15, 20], [15, 20]],
-        [[20, 30], [25, 30, false], [5, 10]],
-        [[30, 35], [10, 15], [35, 61]],
-        [[5, 35], [9]],
-        [[1], [6], [3]],
-        [[4], [5], [2]],
-        [[8], [11]],
-        [[7], [12]],
-        [[10], [0, 5]]
-     ]
-     :
-     [
-        [[-1, -1], [-1, -1]],
-        [[45, 50], [45, 50]],
-        [[35, 45], [35, 40, false], [55, 61]],
-        [[30, 35], [50, 55], [35, 61]],
-        [[5, 35], [8]],
-        [[12], [5], [2]],
-        [[3], [4], [1]],
-        [[7], [10]],
-        [[6], [11]],
-        [[9], [0, 5]]
-     ];
-
-
-    if (!changed()) return null;
-
     // ---------------------------------------------------------------------  //
     // functions
     // ---------------------------------------------------------------------  //
 
     function minMatch(minFrom, minTo) { return minFrom <= MINUTE && MINUTE < minTo };
-
-    function changed() {
-        let item;
-        for (let row of cTimes) {
-            item = row.filter(val => val.length > 1 && minMatch(val[0],  val[1]));
-            if (item.length > 0) break;
-        };
-        const val = item.toString();
-        const change = val !== timeVal;
-        if (change) timeVal = val;
-        return change;
-    }
 
     function fmtFace(row, match, val, repl = true) {
         const src = `(${val})`;
@@ -124,27 +70,44 @@ function getFace() {
         return (b == -1 || e == -1) ? '' : row.substring(b + 1, e);
     }
 
-    // ---------------------------------------------------------------------  //
-    // actions
-    // ---------------------------------------------------------------------  //
-
-    let rows = face.map((row, i) => {
-        cTimes[i].forEach(val => {
+    function R(row, t1, t2) {
+        let t = MINUTE <= 35 ? t1 : t2;
+        t.forEach(val => {
             let isHour = (val.length == 1);
             let txt = getText(row);
             if (txt != '') {
                 row = isHour ? hourFMT(row, txt, val) : minFMT(row, txt, val);
             }
         });
-        return row;
-    });
+        return row
+    }
+
+    // ---------------------------------------------------------------------  //
+    // actions
+    // ---------------------------------------------------------------------  //
+
+    let rows = [
+            R("(I T ' S) X (A B O U T) E",    [[-1, -1], [-1, -1]],                   [[-1, -1], [-1, -1]]),
+            R("(A) C (Q U A R T E R) D C",    [[15, 20], [15, 20]],                   [[45, 50], [45, 50]]),
+            R("(T W E N T Y) (F I V E) X",    [[20, 30], [25, 30, false], [5, 10]],   [[35, 45], [35, 40, false], [55, 61]]),
+            R("(H A L F) B (T E N) F (T O)",  [[30, 35], [10, 15], [35, 61]],         [[30, 35], [50, 55], [35, 61]]),
+            R("(P A S T) E R U (N I N E)",    [[5, 35], [9]],                         [[5, 35], [8]]),
+            R("(O N E) (S I X) (T H R E E)",  [[1], [6], [3]],                        [[12], [5], [2]]),
+            R("(F O U R) (F I V E) (T W O)",  [[4], [5], [2]],                        [[3], [4], [1]]),
+            R("(E I G H T) (E L E V E N)",    [[8], [11]],                            [[7], [10]]),
+            R("(S E V E N) (T W E L V E)",    [[7], [12]],                            [[6], [11]]),
+            R("(T E N) S E (O C L O C K)",    [[10], [0, 5]],                         [[9], [0, 5]])
+    ];
 
     return fmtFaceLines(rows);
 }
 
 function run() {
     let val = getFace();
-    if (val) document.getElementById('clock').innerHTML = val;
+    if (val && timeVal !== val) {
+        document.getElementById('clock').innerHTML = val;
+        timeVal = val;
+    }
     setTimeout(run, 15000);
 }
 

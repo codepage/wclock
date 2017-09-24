90ad0d487279bdd676ad83f01b6f6e999c9489bb
xdaks <xdaks@seznam.cz>
2017-09-24 20:37:41 +0200

    code refactoring

--- d/wclock.d
+++ d/wclock.d
@@ -23,42 +23,43 @@ import std.datetime;
 import std.format;
 import std.array;
 import std.string;
-// import std.regex;
 
-struct TRS {
-    int From;
-    int To;
-    bool Repl;
-    bool IsHr = false;
+struct TimeRangeStruct {
+    int From,  To;
+    bool Repl, IsHour;
 }
 
-// auto re = regex(`\((.*?)\)`);
+alias trs = TimeRangeStruct;
 
-auto mir = (int f, int t, bool r = true) => TRS(f, t, r);
-auto hr  = (int f) => TRS(f, 0, true, true);
-
-string[] face = [
-    "(I T ' S) X (A B O U T) E",
-    "(A) C (Q U A R T E R) D C",
-    "(T W E N T Y) (F I V E) X",
-    "(H A L F) B (T E N) F (T O)",
-    "(P A S T) E R U (N I N E)",
-    "(O N E) (S I X) (T H R E E)",
-    "(F O U R) (F I V E) (T W O)",
-    "(E I G H T) (E L E V E N)",
-    "(S E V E N) (T W E L V E)",
-    "(T E N) S E (O C L O C K)"
-];
-
+trs rs(int f, int t, bool r = true) {return trs(f, t, r);}
+trs rs(int f) {return trs(f, 0, true, true);}
 
 int HOUR;
 int MINUTE;
+string[] face;
+string fmt_on;
+string fmt_off;
+string Color1;
+string EndColor;
 
-string startColor = "";
-string endColor = "";
+void init(string[] args) {
+    SysTime currentTime = Clock.currTime();
 
-string fmt_on = "";
-string fmt_off = "";
+    HOUR = currentTime.hour % 12 == 0 ? 12 : currentTime.hour % 12;
+    MINUTE = currentTime.minute;
+
+    if (args.length > 1 && args[1] == "-t") {
+        string Color = "\033[38;5;43m";
+        Color1 = "\033[38;5;66m";
+        EndColor = "\033[0m";
+
+        fmt_on = EndColor ~ Color ~ "%s" ~ EndColor ~ Color1;
+        fmt_off = "%s";
+    } else {
+        fmt_on = "${{color}}%s${{color1}}";
+        fmt_off = "${{color1}}%s${{color1}}";
+    }
+}
 
 string fmtFace(string row, bool match, string val, bool repl) {
     string src = format("(%s)", val);
@@ -70,102 +71,49 @@ string fmtFace(string row, bool match, string val, bool repl) {
     return row;
 }
 
-string minFMT(string row, string val, TRS trs) {
-    bool match = (trs.From <= MINUTE && MINUTE < trs.To) || (trs.From == -1);
-    return fmtFace(row, match, val, trs.Repl);
+string rowFmt(string row, string val, trs t) {
+    bool match = t.IsHour ?
+                (t.From == HOUR) || (t.From == 0) :
+                (t.From <= MINUTE && MINUTE < t.To) || (t.From == -1);
+    return fmtFace(row, match, val, t.Repl);
 }
 
-string hourFMT(string row, string val, TRS trs) {
-    bool match = (trs.From == HOUR) || (trs.From == 0);
-    return fmtFace(row, match, val, trs.Repl);
-}
-
-string getText(string row) {
+string get_text(string row) {
     long b = indexOf(row, "(");
     long e = indexOf(row, ")");
     return (b == -1 || e == -1) ? "" : row[b + 1..e];
 }
 
-string getClockFace() {
-    return startColor ~ face.join('\n') ~ endColor;
-}
-
-/*
-string get_text() {
-    auto c = matchFirst(face, re);
-    return (!c.empty) && (c.length > 1) ? c[1] : "";
-}
-*/
-
-void getFace() {
-
-    TRS[][] CTimes;
-    if (MINUTE < 35) {
-        CTimes = [
-            [mir(-1, -1), mir(-1, -1)],
-            [mir(15, 20), mir(15, 20)],
-            [mir(20, 30), mir(25, 30, false), mir(5, 10)],
-            [mir(30, 35), mir(10, 15), mir(35, 61)],
-            [mir(5, 35), hr(9)],
-            [hr(1), hr(6), hr(3)],
-            [hr(4), hr(5), hr(2)],
-            [hr(8), hr(11)],
-            [hr(7), hr(12)],
-            [hr(10), mir(0, 5)]
-            ];
-    } else {
-        CTimes = [
-            [mir(-1, -1), mir(-1, -1)],
-            [mir(45, 50), mir(45, 50)],
-            [mir(35, 45), mir(35, 40, false), mir(55, 61)],
-            [mir(30, 35), mir(50, 55), mir(35, 61)],
-            [mir(5, 35), hr(8)],
-            [hr(12), hr(5), hr(2)],
-            [hr(3), hr(4), hr(1)],
-            [hr(7), hr(10)],
-            [hr(6), hr(11)],
-            [hr(9), mir(0, 5)]
-        ];
-    }
-
+string R(string row, trs[] t1, trs[] t2) {
+    auto t = (MINUTE < 35) ? t1 : t2;
     string txt;
-    foreach (i, string row; face) {
-        foreach (TRS val; CTimes[i]) {
-            txt = getText(row);
-            if (txt != "") {
-                row = val.IsHr ? hourFMT(row, txt, val) : minFMT(row, txt, val);
-            }
-        }
-        face[i] = row;
+
+    foreach (i, trs val; t) {
+        txt = get_text(row);
+        if (txt != "") row = rowFmt(row, txt, val);
     }
-    writeln(getClockFace());
+    return row;
 }
 
-void init(string[] args) {
-
-    SysTime currentTime = Clock.currTime();
-    HOUR = currentTime.hour % 12 == 0 ? 12 : currentTime.hour % 12;
-    MINUTE = currentTime.minute;
-
-    if (args.length > 1 && args[1] == "-t") {
-
-        string Color = "\033[38;5;43m";
-        string Color1 = "\033[38;5;66m";
-        string endColor = "\033[0m";
-
-        fmt_on = endColor ~ Color ~ "%s" ~ endColor ~ Color1;
-        fmt_off = "%s";
-
-        startColor = Color1;
-        endColor = endColor;
-    } else {
-
-        fmt_on = "${{color}}%s${{color1}}";
-        fmt_off = "${{color1}}%s${{color1}}";
-    }
+string get_face(string[] rows) {
+    return Color1 ~ rows.join("\n") ~ EndColor;
 }
 
 void main(string[] args) {
     init(args);
-    getFace();
+
+    auto rows = [
+        R("(I T ' S) X (A B O U T) E",      [rs(-1, -1), rs(-1, -1)],                     [rs(-1, -1), rs(-1, -1)]),
+        R("(A) C (Q U A R T E R) D C",      [rs(15, 20), rs(15, 20)],                     [rs(45, 50), rs(45, 50)]),
+        R("(T W E N T Y) (F I V E) X",      [rs(20, 30), rs(25, 30, false), rs(5, 10)],   [rs(35, 45), rs(35, 40, false), rs(55, 61)]),
+        R("(H A L F) B (T E N) F (T O)",    [rs(30, 35), rs(10, 15), rs(35, 61)],         [rs(30, 35), rs(50, 55), rs(35, 61)]),
+        R("(P A S T) E R U (N I N E)",      [rs(5, 35), rs(9)],                           [rs(5, 35), rs(8)]),
+        R("(O N E) (S I X) (T H R E E)",    [rs(1), rs(6), rs(3)],                        [rs(12), rs(5), rs(2)]),
+        R("(F O U R) (F I V E) (T W O)",    [rs(4), rs(5), rs(2)],                        [rs(3), rs(4), rs(1)]),
+        R("(E I G H T) (E L E V E N)",      [rs(8), rs(11)],                              [rs(7), rs(10)]),
+        R("(S E V E N) (T W E L V E)",      [rs(7), rs(12)],                              [rs(6), rs(11)]),
+        R("(T E N) S E (O C L O C K)",      [rs(10), rs(0, 5)],                           [rs(9), rs(0, 5)])
+    ];
+
+    writeln(get_face(rows));
 }
